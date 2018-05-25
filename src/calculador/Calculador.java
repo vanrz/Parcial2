2320f21d9ef9a72c1bdeb554f25ce9bff93abbf4
Estudiantes <Estudiantes@10.20.151.146>
2018-05-25 11:55:37 -0500

    calculador

---
+++ src/calculador/Calculador.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author Estudiantes
+ */
+abstract public class Calculador implements lCalculador {
+
+    
+    
+    Punto punto1;
+    Punto punto2;
+    
+    
+     @Override
+    public double calcularDistancia() {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double calcularArea() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double calcularPendiente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+   }
+    
+    
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    
+    
+
---
+++ src/calculador/Punto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Punto {
+    private int x;
+    private int y;
+    
+    public Punto (double x, double y){
+    this.x=(int) x;
+    this.y=(int) y;
+    }
+    
+     public void setX( int n) {
+        this.x = n;
+    }
+    
+    public int getX(){
+        return x;
+    }
+
+    public void setY(int m) {
+        this.y = m;
+    }
+    
+    public int getY(){
+        return y;
+    }
+
+    public double getPunto() {
+        double Punto = 0;
+        return Punto;
+    }
+    
+    public void setPunto(int n, int m) {
+        this.x=n;
+        this.y=m;
+        
+    }
+
+   
+
+    
+}
