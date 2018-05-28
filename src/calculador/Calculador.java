a4e10426e686da1c66565914e00b6d2a649d40ca
Vanessa Ramirez <36080491+vanrz@users.noreply.github.com>
2018-05-28 16:49:31 -0500

    Update Calculador.java

--- src/calculador/Calculador.java
+++ src/calculador/Calculador.java
@@ -13,18 +13,22 @@ abstract public class Calculador implements lCalculador {
 
     
     
-    Punto punto1;
-    Punto punto2;
+    Punto punto1= new Punto();
+    Punto punto2= new Punto();
     
     
      @Override
     public double calcularDistancia() {
+        
+        
         
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public double calcularArea() {
+        int dx=(punto2.getX()-punto1.getX())*punto1.getY();
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
